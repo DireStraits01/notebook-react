e1a1f8fc7f559b62a04917df32e17faed5100d4a
DireStraits01 <arsamak88@gmail.com>
2022-12-18 11:43:15 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Popups from './components/Popups';
+let cardId = 0;
 
 function App() {
   const [cards, setCards] = useState([]);
   const [isPopusOpen, setIsPopusOpen] = useState(false);
 
+  const createUniqueId = () => {
+    return cardId++;
+  };
   const onCancel = () => setIsPopusOpen(false);
   console.log();
   const handleAddNote = (title, text, time) =>
     setCards(
-      (card) => [...card, { title: title, text: text, time: time }],
-      onCancel()
+      (card) => [
+        ...card,
+        { id: createUniqueId(), title: title, text: text, time: time },
+      ],
+      onCancel(),
+      console.log(cards)
     );
 
   return (
